0f3d2129ef27654337b3a57a20abcf5fd28435d3
Apple <ansita6050@gmail.com>
2025-06-05 11:27:22 +0300

    further modification

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,6 +1,6 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Button from "./components/button"
-import Header from "./components/header"
+import Header from "./components/Header"
 
 const  App = () => {
   return (
--- my-project/src/components/button.jsx
+++ my-project/src/components/button.jsx
@@ -1,6 +1,6 @@
 import ButtonSvg from "../assets/svg/ButtonSvg"
 
-const button = ({className, href, onClick, children, px, white}) => {
+const Button = ({className, href, onClick, children, px, white}) => {
   const classes = `button relative infline-flex 
   items-center justify-center h-11 transition-colors 
   hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} 
@@ -9,7 +9,7 @@ const button = ({className, href, onClick, children, px, white}) => {
   const spanClasses = "relative z-10";
   
     const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
     <span className="{spanClasses}">{children}</span>
     {ButtonSvg(white)}
 </button>
@@ -25,5 +25,5 @@ const renderLink = () => (
 return href ? renderLink() : renderButton();
 };
 
-export default button
+export default Button
 
